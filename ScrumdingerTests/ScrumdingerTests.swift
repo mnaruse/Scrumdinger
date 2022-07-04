ffec168a95f717a2a1b983469e5eca5b27011a3f
Miharu Naruse <nars024@gmail.com>
2022-07-04 17:27:26 +0900

    :recycle: refactor: Apply SwiftFormat

--- ScrumdingerTests/ScrumdingerTests.swift
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -5,11 +5,10 @@
 //  Created by MiharuNaruse on 2022/07/04.
 //
 
-import XCTest
 @testable import Scrumdinger
+import XCTest
 
 class ScrumdingerTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ class ScrumdingerTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
