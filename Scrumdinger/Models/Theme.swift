4df3d7f6439b2698b3f13717bc0110d7ae78011c
Miharu Naruse <nars024@gmail.com>
2022-07-05 13:15:48 +0900

    :sparkles: feat: Creating a Card View > Create a Color Theme

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,47 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by MiharuNaruse on 2022/07/05.
+//
+
+import SwiftUI
+
+/// カラーテーマ
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+
+    /// アクセントカラー
+    ///
+    /// テーマのメインカラーに比べてハイコントラストな色。
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow:
+            return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple:
+            return .white
+        }
+    }
+
+    /// メインカラー
+    ///
+    /// アセットカタログから色を初期化する。
+    var mainColor: Color {
+        return Color(rawValue)
+    }
+}
