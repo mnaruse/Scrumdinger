da6427b62c746d8bd0298aae335455a57d3d18d4
Miharu Naruse <nars024@gmail.com>
2022-07-13 09:35:36 +0900

    :sparkles: feat: State Management - Managing State and Life Cycle > Add Design Elements to the Meeting Header

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -12,6 +12,7 @@ struct MeetingHeaderView: View {
 
     let secondsElapsed: Int
     let secondsRemaining: Int
+    let theme: Theme
 
     // MARK: Private Computed Properties
 
@@ -35,6 +36,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -46,18 +48,20 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel("Time Remaining")
             .accessibilityValue("\(minutesRemaining) minutes")
         }
+        .padding([.top, .horizontal])
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 300, secondsRemaining: 600)
+        MeetingHeaderView(secondsElapsed: 300, secondsRemaining: 600, theme: .bubblegum)
             .previewLayout(.sizeThatFits)
     }
 }
