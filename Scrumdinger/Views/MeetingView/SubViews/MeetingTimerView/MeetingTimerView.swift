0b6ee7cfc9e0d418e94e679c1cc6be9e538e9a88
Miharu Naruse <nars024@gmail.com>
2022-08-16 08:28:25 +0900

    :sparkles: feat: Recording Audio - Transcribing Speech to Text > Display Recording Indicators

--- Scrumdinger/Views/MeetingView/SubViews/MeetingTimerView/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingView/SubViews/MeetingTimerView/MeetingTimerView.swift
@@ -11,6 +11,7 @@ struct MeetingTimerView: View {
     // MARK: Internal Stored Properties
 
     let speakers: [ScrumTimer.Speaker]
+    let isRecording: Bool
     let theme: Theme
 
     // MARK: Private Computed Properties
@@ -29,6 +30,10 @@ struct MeetingTimerView: View {
                     Text(currentSpeaker)
                         .font(.title)
                     Text("is speaking")
+                    Image(systemName: isRecording ? "mic" : "mic.slash")
+                        .font(.title)
+                        .padding(.top)
+                        .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
                 }
                 .accessibilityElement(children: .combine)
                 .foregroundStyle(theme.accentColor)
@@ -55,6 +60,6 @@ struct MeetingTimerView_Previews: PreviewProvider {
     }
 
     static var previews: some View {
-        MeetingTimerView(speakers: speakers, theme: .yellow)
+        MeetingTimerView(speakers: speakers, isRecording: true, theme: .yellow)
     }
 }
